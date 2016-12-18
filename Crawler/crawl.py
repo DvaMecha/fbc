b9c64c53a9d428628a756f112840762a99ddbd09
Rolland Chen <rolland_chen@126.com>
2016-12-18 21:57:22 +0800

    无
    5

--- Crawler/crawl.py
+++ Crawler/crawl.py
@@ -1,5 +1,11 @@
 import requests
+import Model.MainModel
+import time
+from bs4 import BeautifulSoup
+from Crawler.CrawlRule import *
 
+model = Model.MainModel.MainModel.GetInstance()
+defurl = 'http://odds.500.com/'
 
 
 def creatWebHeard():
@@ -21,16 +27,57 @@ def getDatas(url):
     return encode_content
 
 
-def getWebInfo(url):
-    req = creatWebHeard()
-    r = requests.get(url, headers=req)
-    print(type(r))
-    print(r.status_code)
-    print(r.encoding)
-    # print r.text
-    print(r.cookies)
-    info = {
-        'encoding': r.encoding,
-        'info': r.cookies,
-        'code': r.status_code}
-    return info
+class Crawler():
+
+    def __init__(self):
+        self.ruleFun = {'odds500': ruleBy500, 'list': self.getMatchList}
+        pass
+
+    def runCrawler(self, t):
+        model.view['main'].outPutText(
+            model.datamodel['info']['start'], True)
+        model.startTime = time.time()
+        if model.crawlLock:
+            model.view['main'].outPutText(model.datamodel['info']['start'], True)
+            return
+        model.crawlLock = True
+        d = getDatas(defurl)
+        soup = BeautifulSoup(d, 'html5lib')
+        obj = self.ruleFun[t](soup)
+        model.getData[t]=obj
+        return obj
+
+    def getAllodds(self):
+        pass
+
+    def getMatchList(self, soup):
+        obj = {}
+        # obj['odd'] = soup.find_all("tbody", attrs={"id": "main-tbody"})
+        flag = False
+        for item in model.datamodel['league'].items():
+            lg = soup.find_all("tr", attrs={"data-mid": item[1]['mid_500']})
+            lgid = item[0]
+            obj[lgid] = {}
+            obj[lgid]["lgname"] = item[1]['name']
+            obj[lgid]['matchs'] = []
+            if len(lg) == 0:
+                model.view['main'].outPutText(
+                    obj[lgid]["lgname"] + model.datamodel['info']['lgnomatch'])
+                continue
+            else:
+                flag = True
+                for i in lg:
+                    matchId = i.get("data-fid")
+                    obj[lgid]['matchs'].append(matchId)
+                    matchInfo = {
+                        "t1": i.select(".text_right > a")[0].get_text(),
+                        "t2": i.select(".text_left > a")[0].get_text(),
+                        "lc": i.select("td")[2].get_text()}
+                    obj[lgid][matchId] = {'info': matchInfo}
+                    t = "[" + obj[lgid]["lgname"] + "][" + matchInfo["lc"] + \
+                        "]" + matchInfo["t1"] + " vs " + matchInfo["t2"] + ":"
+                    model.view['main'].outPutText(t + '\n')
+
+        if not flag:
+            model.view['main'].outPutText(model.datamodel['info']['nomatch'], True)
+        return obj
